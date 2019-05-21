817247de6b194c9b9dbd289802d868be9893b39a
ekam230 <ekam230@gmail.com>
2019-05-21 12:46:09 +0400

    some fix and scrollup

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,25 @@ import bootstrap from 'bootstrap';
 // jQuery(function () {
 //     jQuery('body').css('color', 'blue');
 // });
+
+jQuery(function () {
+    // при нажатии на кнопку scrollup
+    jQuery('.scrollup').click(function () {
+        // переместиться в верхнюю часть страницы
+        jQuery("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+    })
+})
+// при прокрутке окна (window)
+jQuery(window).scroll(function () {
+    // если пользователь прокрутил страницу более чем на 200px
+    if (jQuery(this).scrollTop() > 200) {
+        // то сделать кнопку scrollup видимой
+        jQuery('.scrollup').fadeIn();
+    }
+    // иначе скрыть кнопку scrollup
+    else {
+        jQuery('.scrollup').fadeOut();
+    }
+});
